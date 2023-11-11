4aec417220f55e1f6ab8bc4094fa98eb189b44a5
Yash Shah <shayash41@gmail.com>
2023-11-11 11:58:35 -0500

    setup login page and api routes

--- README.md
+++
@@ -1,1 +0,0 @@
-# hackprinceton2023
---
+++ my-app/src/app/api/register/route.ts
@@ -0,0 +1,26 @@
+import User from "@/models/User";
+import connect from "@/utils/db";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const POST = async (request: any) => {
+  const { email, password } = await request.json();
+
+  connect();
+
+  const hashedPassword = await bcrypt.hash(password, 5);
+  const newUser = new User({
+    email,
+    password: hashedPassword,
+    proficiencyScore: 0.5,
+  });
+
+  try {
+    await newUser.save();
+    return new NextResponse("user is registered", { status: 200 });
+  } catch (err: any) {
+    return new NextResponse(err, {
+      status: 500,
+    });
+  }
+};
