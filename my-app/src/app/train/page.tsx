d6a56d13754f73e8bca25c5df0a2513f07dd5a0d
Yash Shah <shayash41@gmail.com>
2023-11-12 02:08:48 -0500

    updated mp3 file (i think?)

--- my-app/src/app/train/page.tsx
+++ my-app/src/app/train/page.tsx
@@ -4,31 +4,58 @@ import React, { useState } from 'react';
 const AudioUploadButton = () => {
     const [file, setFile] = useState(null);
     const [pdfUrl, setPdfUrl] = useState('http://localhost:3000/my_music.pdf');
+    const [score, setScore] = useState(''); // State to store the score
 
     const handleFileChange = async (event: any) => {
         const uploadedFile = event.target.files[0];
         if (uploadedFile) {
-            setFile(uploadedFile.name);
+            setFile(uploadedFile);
             console.log('Audio file uploaded:', uploadedFile.name);
 
-            // Create FormData
+            // Create FormData for the first POST request
             const formData = new FormData();
             formData.append('audio', uploadedFile);
 
-            // POST request with FormData
+            // POST request with FormData to upload the audio file
             try {
-                const response = await fetch('YOUR_ENDPOINT_URL', {
+                const uploadResponse = await fetch('YOUR_UPLOAD_ENDPOINT_URL', {
                     method: 'POST',
                     body: formData,
                 });
-                const data = await response.json();
-                console.log('Server response:', data);
+                const uploadData = await uploadResponse.json();
+                console.log('Server response from upload:', uploadData);
+
+                // Set score from the upload response if it's included
+                if (uploadData.score) {
+                    setScore(uploadData.score);
+
+                    // Create a JSON object for the second POST request
+                    const proficiencyData = {
+                        score: uploadData.score,
+                        // Include any other data required by your endpoint
+                    };
+
+                    // Second POST request to updateProficiency
+                    const updateResponse = await fetch('YOUR_UPDATE_PROFICIENCY_ENDPOINT_URL', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(proficiencyData),
+                    });
+
+                    if (updateResponse.ok) {
+                        console.log('Proficiency score updated successfully');
+                        // Additional logic for a successful update can go here
+                    } else {
+                        console.error('Failed to update proficiency score', updateResponse.statusText);
+                    }
+                }
             } catch (error) {
-                console.error('Error uploading file:', error);
+                console.error('Error during the process:', error);
             }
         }
     };
-
     return (
         <div>
             {pdfUrl && (
@@ -55,7 +82,16 @@ const AudioUploadButton = () => {
                 >
                     Upload Audio
                 </label>
-                {file && <p>File selected: {file}</p>}
+                {file && <p>File selected: {file.name}</p>}
+                {/* Textbox to display the score */}
+                {score && (
+                    <input 
+                        type="text" 
+                        value={`Score: ${score}`} 
+                        readOnly 
+                        className="mt-2 text-center border-2 border-blue-500 rounded"
+                    />
+                )}
             </div>
         </div>
     );
