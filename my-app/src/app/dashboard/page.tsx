4360974b6da26c4a74f890e024eab465cc92f815
Yash Shah <shayash41@gmail.com>
2023-11-11 22:20:34 -0500

    fixed some styling

--- my-app/src/app/dashboard/page.tsx
+++ my-app/src/app/dashboard/page.tsx
@@ -1,21 +1,36 @@
-import React from "react";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+'use client'
+import React, { useState } from 'react';
 
+const AudioUploadButton = () => {
+    const [file, setFile] = useState(null);
 
-const Dashboard = async () => {
-  const session = await getServerSession();
-  if (!session) {
-    
-    redirect("/");
-  }
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      Dashboard
-    </div>
-  );
+    const handleFileChange = (event: any) => {
+        const uploadedFile = event.target.files[0];
+        if (uploadedFile) {
+            setFile(uploadedFile);
+            console.log('Audio file uploaded:', uploadedFile.name);
+            // You can handle the file upload logic here
+        }
+    };
+
+    return (
+        <div>
+            <input 
+                type="file" 
+                id="audio-upload" 
+                accept="audio/*" 
+                onChange={handleFileChange} 
+                hidden 
+            />
+            <label 
+                htmlFor="audio-upload" 
+                className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Upload Audio
+            </label>
+            {file && <p>File selected: {file.name}</p>}
+        </div>
+    );
 };
 
-
-
-export default Dashboard;
+export default AudioUploadButton;
